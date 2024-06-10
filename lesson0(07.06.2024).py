99756dcbddc4143859f1db37fb8b55d486dbfe1d
Bakhtiyar Tukhtasinov <144336544+tuxtasinow@users.noreply.github.com>
2024-06-10 11:49:04 +0500

    Add files via upload

---
+++ lesson0(07.06.2024).py
@@ -0,0 +1,26 @@
+#Задачи
+
+a = 'python'  #Длинна слова
+print(len(a))
+
+first = 10  #Суммы и разности
+second = 2
+summa = first + second
+diff = first - second
+print(summa, diff)
+
+first = 10  #Среднее арифметическое
+second = 2
+third = 4
+mean = (first + second + third) / 3
+print(mean)
+
+first_string = 'Вторник'  #Простые строчки
+second_string = 'Понедельник'
+print(second_string + ', ' + first_string)
+
+a = 10  #Сложная формула
+b = 2
+c = 4
+f = (a * b) + (a * c)
+print(f**2 / 2)
